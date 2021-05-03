e0154147ec84e1af43ecd54b918eb5db38b0116c
PawankumarPK <pk9910765616@gmail.com>
2021-05-04 01:22:34 +0530

    add dont have account text with signup and referect dont have account code

--- lib/screens/Welcome/login/component/body.dart
+++ lib/screens/Welcome/login/component/body.dart
@@ -1,5 +1,6 @@
 import 'package:agami_dummy/component/rounded_button.dart';
 import 'package:agami_dummy/constant.dart';
+import 'package:agami_dummy/screens/Welcome/login/component/already_have_an_account_check.dart';
 import 'package:agami_dummy/screens/Welcome/login/component/background.dart';
 import 'package:agami_dummy/screens/Welcome/login/component/round_input_field.dart';
 import 'package:agami_dummy/screens/Welcome/login/component/rounded_password_field.dart';
@@ -20,25 +21,27 @@ class Body extends StatelessWidget {
               "LOGIN",
               style: TextStyle(fontWeight: FontWeight.bold),
             ),
+            SizedBox(height: size.height * 0.03),
             SvgPicture.asset(
               "assets/icons/login.svg",
               height: size.height * 0.35,
             ),
+            SizedBox(height: size.height * 0.03),
             RoundedInputField(
               hintText: "Your Email",
               onChanged: (value) {},
             ),
-            RoundedPasswordField(onChanged: (value){},
+            RoundedPasswordField(
+              onChanged: (value) {},
             ),
             RoundedButton(
               text: "LOGIN",
-              press: (){},
+              press: () {},
             ),
+            AlreadyHaveAnAccountCheck()
           ],
         ),
       ),
     );
   }
 }
-
-
