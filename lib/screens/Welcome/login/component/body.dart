9a2621f17b813af685c9326db78ea41670be5e10
PawankumarPK <pk9910765616@gmail.com>
2021-05-04 00:44:35 +0530

    login main image added

---
+++ lib/screens/Welcome/login/component/body.dart
@@ -0,0 +1,51 @@
+import 'package:agami_dummy/constant.dart';
+import 'package:agami_dummy/screens/Welcome/login/component/background.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class Body extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Background(
+      child: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              "LOGIN",
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            SvgPicture.asset(
+              "assets/icons/login.svg",
+              height: size.height * 0.35,
+            ),
+            TextFieldContainer()
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class TextFieldContainer extends StatelessWidget {
+  final Widget child;
+
+  const TextFieldContainer({
+    Key key,
+    this.child,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+      width: size.width * 0.8,
+      decoration: BoxDecoration(
+          color: kPrimaryLightColor, borderRadius: BorderRadius.circular(29)
+      ),
+      child: child,
+    );
+  }
+}
