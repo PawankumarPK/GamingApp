8bae6dcfb5d204f7b8eb7f06e9aff4d23f3accea
PawankumarPK <pk9910765616@gmail.com>
2021-05-03 21:36:21 +0530

    home page done

--- lib/screens/Welcome/component/body.dart
+++ lib/screens/Welcome/component/body.dart
@@ -1,3 +1,4 @@
+import 'package:agami_dummy/component/rounded_button.dart';
 import 'package:agami_dummy/constant.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
@@ -8,47 +9,34 @@ class Body extends StatelessWidget {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Background(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Text(
-            "Welcome TO EDU",
-            style: TextStyle(fontWeight: FontWeight.bold),
-          ),
-          SvgPicture.asset(
-            "assets/icons/chat.svg",
-            height: size.height * 0.45,
-          ),
-          RoundedButton()
-        ],
+      child: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              "Welcome TO EDU",
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: size.height * 0.05,),
+            SvgPicture.asset(
+              "assets/icons/chat.svg",
+              height: size.height * 0.45,
+            ),
+            SizedBox(height: size.height * 0.05,),
+            RoundedButton(
+              text: "LOGIN",
+              press: () {},
+            ),
+            RoundedButton(
+              text: "SIGNUP",
+              textColor: Colors.black,
+              color: kPrimaryLightColor,
+              press: () {},
+            )
+          ],
+        ),
       ),
     );
   }
 }
 
-class RoundedButton extends StatelessWidget {
-  const RoundedButton({
-    Key key,
-  }) : super(key: key);
-
-
-
-  @override
-  Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
-    return Container(
-      width: size.width * 0.8,
-      child: ClipRRect(
-        borderRadius: BorderRadius.circular(29),
-        child: FlatButton(
-            padding: EdgeInsets.symmetric(vertical: 20, horizontal: 40),
-            color: kPrimaryColor,
-            onPressed: () {},
-            child: Text(
-              "LOGIN",
-              style: TextStyle(color: Colors.white),
-            )),
-      ),
-    );
-  }
-}
