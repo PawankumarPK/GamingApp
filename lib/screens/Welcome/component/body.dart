6c95e7d54e0a0011e5907a47a2fff3dab39ee58c
PawankumarPK <pk9910765616@gmail.com>
2021-05-03 20:47:17 +0530

    login button done

--- lib/screens/Welcome/component/body.dart
+++ lib/screens/Welcome/component/body.dart
@@ -1,3 +1,4 @@
+import 'package:agami_dummy/constant.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'background.dart';
@@ -5,6 +6,7 @@ import 'background.dart';
 class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Background(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
@@ -13,9 +15,39 @@ class Body extends StatelessWidget {
             "Welcome TO EDU",
             style: TextStyle(fontWeight: FontWeight.bold),
           ),
-          SvgPicture.asset("assets/icons/chat.svg")
-
+          SvgPicture.asset(
+            "assets/icons/chat.svg",
+            height: size.height * 0.45,
+          ),
+          RoundedButton()
         ],
+      ),
+    );
+  }
+}
+
+class RoundedButton extends StatelessWidget {
+  const RoundedButton({
+    Key key,
+  }) : super(key: key);
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Container(
+      width: size.width * 0.8,
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(29),
+        child: FlatButton(
+            padding: EdgeInsets.symmetric(vertical: 20, horizontal: 40),
+            color: kPrimaryColor,
+            onPressed: () {},
+            child: Text(
+              "LOGIN",
+              style: TextStyle(color: Colors.white),
+            )),
       ),
     );
   }
