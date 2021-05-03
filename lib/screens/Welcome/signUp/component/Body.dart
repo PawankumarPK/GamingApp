7251eb5c300526aa440c2eaee341c171a54f30a3
PawankumarPK <pk9910765616@gmail.com>
2021-05-04 01:43:27 +0530

    sign up screen background done

---
+++ lib/screens/Welcome/signUp/component/Body.dart
@@ -0,0 +1,9 @@
+import 'package:agami_dummy/screens/Welcome/signUp/component/Background.dart';
+import 'package:flutter/material.dart';
+
+class Body extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Background();
+  }
+}
--- lib/screens/Welcome/signUp/signup.dart
+++ lib/screens/Welcome/signUp/signup.dart
@@ -1,8 +1,11 @@
+import 'package:agami_dummy/screens/Welcome/signUp/component/Body.dart';
 import 'package:flutter/material.dart';
 
 class SignUp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Body(),
+    );
   }
 }
